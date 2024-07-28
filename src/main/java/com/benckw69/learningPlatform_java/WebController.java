aa3e8c751d6738916f3dc912392ce63d4cf37658
benckw69 <166276425+benckw69@users.noreply.github.com>
2024-07-28 16:02:39 +0800

    Now session functions are set.  Spring session module does not have usage

--- src/main/java/com/benckw69/learningPlatform_java/WebController.java
+++ src/main/java/com/benckw69/learningPlatform_java/WebController.java
@@ -1,6 +1,7 @@
 package com.benckw69.learningPlatform_java;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,25 +14,49 @@ import jakarta.servlet.http.HttpSession;
 public class WebController {
 
     @ModelAttribute("title")
-    public String messages() {
+    public String title() {
         Global global = new Global();
         return global.getTitle();
+    }
+
+    @ModelAttribute("messages")
+    public List<String> messages(HttpSession session) {
+        List<String> messages = null;
+        if(session.getAttribute("messages") != null){
+            messages = (List<String>)session.getAttribute("messages");
+            session.removeAttribute("messages");
+        }
+        return messages;
     }
 
     
     @GetMapping("/")
     public String index(HttpSession session){
+        //this sentence need to be remove.  Just illustration
         session.setAttribute("user", "teacher");
         return "pages/index";
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login(HttpSession session, Model model){
+        
+        List<String> arraylist = new ArrayList<>();
+        arraylist.add("123");
+        arraylist.add("456");
+        session.setAttribute("messages", arraylist);
+
         return "pages/login";
     }
 
     @GetMapping("/register")
-    public String register(){
+    public String register(HttpSession session, Model model){
         return "pages/register";
+        
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.removeAttribute("user");
+        return "redirect:/login";
     }
 }
