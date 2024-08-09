b0a4642b46f03127502755e344dc1199f3e1f6e2
benckw69 <166276425+benckw69@users.noreply.github.com>
2024-08-09 21:20:14 +0800

    Minor changes

--- src/main/java/com/benckw69/learningPlatform_java/AdminConfig/MoneyRecord.java
+++ src/main/java/com/benckw69/learningPlatform_java/AdminConfig/MoneyRecord.java
@@ -5,7 +5,9 @@ import java.sql.Timestamp;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.benckw69.learningPlatform_java.Course.Course;
 import com.benckw69.learningPlatform_java.MoneyTicket.MoneyTicket;
+import com.benckw69.learningPlatform_java.User.Type;
 import com.benckw69.learningPlatform_java.User.User;
 
 import jakarta.persistence.Column;
@@ -108,25 +110,44 @@ public class MoneyRecord {
     }
 
     //referral bonus for new user
-    public void setEventText(EventCategory eventCategory, User user, User user2, Referral referral){
+    public void setEventText(EventCategory eventCategory, User newUser, User referral, Referral referralSetting){
         if(eventCategory == EventCategory.REFERRAL_BONUS){
-            this.eventText = "用戶 "+user.getUsername()+" 獲得新用戶獎勵 $"+referral.getNewUserAmount()+"，推薦者為用戶 "+user2.getUsername()+"。";
+            this.eventText = "用戶 "+newUser.getUsername()+"，編號為 "+newUser.getId()+" 獲得新用戶獎勵 $"+referralSetting.getNewUserAmount()+"，推薦者為用戶 "+referral.getUsername()+"，編號為 "+referral.getId()+"。";
         }
     }
 
     //referral bonus for referral
-    public void setEventText(EventCategory eventCategory, User user, Referral referral, User user2){
+    public void setEventText(EventCategory eventCategory, User referral, Referral referralSetting, User newUser){
         if(eventCategory == EventCategory.REFERRAL_BONUS){
-            this.eventText = "用戶 "+user.getUsername()+" 獲得推薦獎勵 $"+referral.getReferralAmount()+"，新用戶為 "+user2.getUsername()+"。";
+            this.eventText = "用戶 "+referral.getUsername()+"，編號為 "+referral.getId()+" 獲得推薦獎勵 $"+referralSetting.getReferralAmount()+"，新用戶為 "+newUser.getUsername()+"，編號為 "+newUser.getId()+"。";
         }
     }
 
-    //
+    //add money to
     public void setEventText(EventCategory eventCategory, User user, MoneyTicket moneyTicket){
         if(eventCategory == EventCategory.ADD_MONEY){
-            this.eventText = "用戶 "+user.getUsername()+" 増值 $"+moneyTicket.getAmount()+"，號碼為 "+moneyTicket.getTicketString()+"。";
+            this.eventText = "用戶 "+user.getUsername()+"，編號為 "+user.getId()+"，増值 $"+moneyTicket.getAmount()+"，號碼為 "+moneyTicket.getTicketString()+"，編號為 "+moneyTicket.getId()+"。";
         }
     }
 
     //three more conditions for buying course
+    public void setEventText(EventCategory eventCategory, User student, Course course){
+        if(eventCategory == EventCategory.BUY_COURSE){
+            if(user.getType() == Type.student){
+                 this.eventText = "用戶 "+student.getUsername()+"，編號為 "+student.getId()+"，購買了編號為 "+course.getId()+"，名稱為 "+course.getTitle()+" 的課程，付費了 $"+course.getPrice()+"。";
+            }
+        }
+    }
+
+    public void setEventText(EventCategory eventCategory, User student, User teacherOrAdmin, Course course, MoneySeperation moneySeperation){
+        if(eventCategory == EventCategory.BUY_COURSE){
+            Integer teacherMoney = course.getPrice() * moneySeperation.getTeacherMoneyPercentage() /100;
+            Integer adminMoney = course.getPrice() - teacherMoney;
+            if(user.getType() == Type.admin){
+                this.eventText = "用戶 "+student.getUsername()+"，編號為 "+student.getId()+"，購買了編號為 "+course.getId()+"，名稱為 "+course.getTitle()+" 的課程，編號為 "+teacherOrAdmin.getId()+"，名稱為 "+teacherOrAdmin.getUsername()+" 的管理員從價錢為 $"+course.getPrice()+" 的課程獲得 "+(100 - moneySeperation.getTeacherMoneyPercentage())+"% 的分成，獲得 $"+adminMoney+"。";
+            } else if (user.getType() == Type.teacher){
+                this.eventText = "用戶 "+student.getUsername()+"，編號為 "+student.getId()+"，購買了編號為 "+course.getId()+"，名稱為 "+course.getTitle()+" 的課程，編號為 "+teacherOrAdmin.getId()+"，名稱為 "+teacherOrAdmin.getUsername()+" 的導師從價錢為 $"+course.getPrice()+" 的課程獲得 "+moneySeperation.getTeacherMoneyPercentage()+"% 的分成，獲得 $"+teacherMoney+"。";
+            }
+        }
+    }
 }
